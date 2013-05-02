d019ca5654ad18bbc60b52bfc47f747252334125
Matt Gale <matt@littleball.co.uk>
2013-05-02 17:55:09 +0100

    reformated files to pass jsHint

--- js/assets/Carousel.js
+++ js/assets/Carousel.js
@@ -4,10 +4,9 @@
  */
 
 define('Carousel', ['Utils', 'Child'], function(Utils, Child) {
-
 	return Utils.create(Child, {
 		init: function() {
-			//console.log('Carousel', this);
+			console.log('Carousel', this);
 		},
 		render: function(params) {
 			this.el.innerHTML = Utils.render(this.tmpl, params);
